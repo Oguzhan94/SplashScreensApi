1904b4a533bde5edc97e689012fae3f986911577
oguzhan <oguzhangunduz1994@gmail.com>
2022-10-06 03:13:03 +0300

    SplashScreenApi

---
+++ app/src/main/java/com/example/splashscreensapi/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.splashscreensapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
+        setContentView(R.layout.activity_main)
+    }
+}
